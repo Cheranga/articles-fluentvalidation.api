0478ba96dc36b1eb130bfcb2220df299a651a912
Cheranga Hatangala <cheranga@gmail.com>
2022-08-08 22:52:06 +1000

    feat: removing commented code.

--- src/Demo.Products.Api/Extensions/ValidationResultExtensions.cs
+++ src/Demo.Products.Api/Extensions/ValidationResultExtensions.cs
@@ -5,25 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Demo.Products.Api.Extensions;
 
 public static class ValidationResultExtensions
-{
-    // public static ProblemDetails ToProblemDetails(ValidationResult validationResult)
-    // {
-    //     if (validationResult.IsValid)
-    //     {
-    //         return null;
-    //     }
-    //     var problemDetails = new ProblemDetails
-    //     {
-    //         Type = "ValidationError",
-    //         Detail = "invalid request, please check the error list for more details",
-    //         Status = (int) (HttpStatusCode.BadRequest),
-    //         Title = "invalid request"
-    //     };
-    //     
-    //     problemDetails.Extensions.Add("errors", validationResult.Errors.ToDictionary(x=>x.PropertyName, x=>x.ErrorMessage));
-    //     return problemDetails;
-    // }
-    
+{   
     public static ProblemDetails ToProblemDetails(this IEnumerable<ValidationFailure> validationFailures)
     {
         var errors = validationFailures.ToDictionary(x => x.PropertyName, x => x.ErrorMessage);
